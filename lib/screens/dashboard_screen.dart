f4188795d1ae67227ed3b249cc1dbeb22bfef6ad
Cheng Yi <chengyiloi@Chengs-MacBook-Pro.local>
2020-12-10 15:33:14 +0800

    Updated bottom navigation bar options

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -2,8 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fil/constants.dart';
 import 'package:percent_indicator/percent_indicator.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-
+import 'package:fl_chart/fl_chart.dart';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -15,40 +14,48 @@ class _DashboardState extends State<Dashboard> {
 
   List<BottomNavigationBarItem> navBarItems = [
     BottomNavigationBarItem(
-      label: "Dashboard",
+        label: "Dashboard",
         icon: Padding(
           padding: navIconPadding,
           child: Image.asset(
-      "images/graph.png",
-      color: navBarBlue,
-    ),
+            "images/graph.png",
+            color: navBarBlue,
+          ),
+        )),
+        BottomNavigationBarItem(
+        label: "Reminders",
+        icon: Padding(
+          padding: navIconPadding,
+          child: Image.asset(
+            "images/reminder.png",
+            color: navBarGrey,
+          ),
         )),
     BottomNavigationBarItem(
-      label: "Explore",
+        label: "Explore",
         icon: Padding(
           padding: navIconPadding,
           child: Image.asset(
-      "images/map.png",
-      color: navBarGrey,
-    ),
+            "images/map.png",
+            color: navBarGrey,
+          ),
         )),
     BottomNavigationBarItem(
-      label: "Recepie",
+        label: "Recepies",
         icon: Padding(
           padding: navIconPadding,
           child: Image.asset(
-      "images/recepie.png",
-      color: navBarGrey,
-    ),
+            "images/recepie.png",
+            color: navBarGrey,
+          ),
         )),
     BottomNavigationBarItem(
-      label: "Logout",
+        label: "Logout",
         icon: Padding(
           padding: navIconPadding,
           child: Image.asset(
-      "images/logout.png",
-  
-    ),
+            "images/logout.png",
+          ),
         )),
   ];
 
@@ -174,18 +181,131 @@ class _DashboardState extends State<Dashboard> {
                   child: Text("Edit Goal"),
                   onPressed: () {}),
             ),
-            Container(
-              height: 240,
-              child: SfCartesianChart())
+            AspectRatio(
+              aspectRatio: 1.7,
+              child: Card(
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4)),
+                color: const Color(0xfff6F6F6),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 12.0),
+                  child: BarChart(
+                    BarChartData(
+                        alignment: BarChartAlignment.spaceEvenly,
+                        maxY: 6,
+                        axisTitleData: FlAxisTitleData(
+                          show: true,
+                          leftTitle: AxisTitle(
+                              showTitle: true,
+                              titleText: "Litres",
+                              textAlign: TextAlign.end,
+                              margin: 0),
+                        ),
+                        gridData: FlGridData(
+                          show: true,
+                          drawHorizontalLine: true,
+                          checkToShowHorizontalLine: (value) {
+                            if (value == 3) {
+                              return true;
+                            }
+                            return false;
+                          },
+                        ),
+                        titlesData: FlTitlesData(
+                          show: true,
+                          bottomTitles: SideTitles(
+                            showTitles: true,
+                            getTextStyles: (value) => TextStyle(
+                              color:
+                                  value == 3 ? Colors.black : Color(0xffD5D5D5),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                            ),
+                            margin: 20,
+                            getTitles: (double value) {
+                              switch (value.toInt()) {
+                                case 0:
+                                  return "Mon";
+                                case 1:
+                                  return "Tue";
+                                case 2:
+                                  return "Wed";
+                                case 3:
+                                  return "Thu";
+                                case 4:
+                                  return "Fri";
+                                case 5:
+                                  return "Sat";
+                                case 6:
+                                  return "Sun";
+                                default:
+                                  return '';
+                              }
+                            },
+                          ),
+                          leftTitles: SideTitles(
+                            showTitles: true,
+                          ),
+                        ),
+                        borderData: FlBorderData(
+                            show: true,
+                            border: Border(
+                              left: BorderSide(width: 1.0, color: Colors.black),
+                              bottom:
+                                  BorderSide(width: 1.0, color: Colors.black),
+                            )),
+                        barGroups: [
+                          BarChartGroupData(x: 0, barRods: [
+                            BarChartRodData(y: 1, colors: [Color(0xff2E80EC)])
+                          ]),
+                          BarChartGroupData(x: 1, barRods: [
+                            BarChartRodData(y: 1.5, colors: [Color(0xff2E80EC)])
+                          ]),
+                          BarChartGroupData(x: 2, barRods: [
+                            BarChartRodData(y: 2, colors: [Color(0xff2E80EC)])
+                          ]),
+                          BarChartGroupData(x: 3, barRods: [
+                            BarChartRodData(y: 2.5, colors: [Color(0xff2E80EC)])
+                          ]),
+                          BarChartGroupData(x: 4, barRods: [
+                            BarChartRodData(y: 0, colors: [Color(0xff2E80EC)])
+                          ]),
+                          BarChartGroupData(x: 5, barRods: [
+                            BarChartRodData(y: 0, colors: [Color(0xff2E80EC)])
+                          ]),
+                          BarChartGroupData(x: 6, barRods: [
+                            BarChartRodData(y: 0, colors: [Color(0xff2E80EC)])
+                          ]),
+                        ]),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CupertinoButton(
+                borderRadius: BorderRadius.circular(50.0),
+                  padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                  child: Image.asset("images/cup.png"),
+                  color: Color(0xff88BDBC),
+                  onPressed: () {}),
+            )
+           
           ],
         ),
       )),
       bottomNavigationBar: CupertinoTabBar(
         items: navBarItems,
-
         currentIndex: 0,
         activeColor: Colors.black,
       ),
     );
   }
+}
+
+class Sales {
+  final String year;
+  final int sales;
+  Sales(this.year, this.sales);
 }
